a7068fd0eda0018d3d54562b1e17cd98375d41af
Niko Cremer <n.ko@posteo.de>
2023-11-08 13:01:49 +0100

    Exercises Done!

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const buttonDark = document.querySelector('[data-js="dark-mode-button"]');
+const buttonWhite = document.querySelector('[data-js="light-mode-button"]');
+const buttonToggle = document.querySelector('[data-js="toggle-button"]');
+
+buttonDark.addEventListener("click", () => {
+  console.log("Click!");
+  bodyElement.classList.add("dark");
+});
+
+buttonWhite.addEventListener("click", () => {
+  console.log("Click!");
+  bodyElement.classList.remove("dark");
+});
+
+buttonToggle.addEventListener("click", () => {
+  console.log("Click!");
+  bodyElement.classList.toggle("dark");
+});
